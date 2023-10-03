d7be3cd0301dbcb9b4295f8a8e96ff8b08f32713
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-10-03 19:00:18 -0300

    Card Appointment

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,9 +1,19 @@
 import { VStack, Text } from "native-base";
+import { CardConsulta } from "../componentes/CardConsulta";
+import { Titulo } from "../componentes/Titulo";
 
 export default function Consultas() {
   return (
-    <VStack>
-      <Text>Consultas</Text>
+    <VStack p={3}>
+      <Titulo color="blue.500">Minhas Consultas</Titulo>
+      <CardConsulta
+        nome="Dr. André Cunha"
+        especialidade="Cardiologista"
+        data="20/10/2023"
+        foto="https://github.com/LorenzoRybuBuglione.png"
+      foiAtendido
+      foiAgendado
+      />
     </VStack>
   );
 }
---
+++ src/componentes/CardConsulta.tsx
@@ -0,0 +1,45 @@
+import { VStack, Text, Avatar } from "native-base";
+import { Botao } from "./Botao";
+
+interface CardProps {
+  nome: string;
+  foto: string;
+  especialidade: string;
+  data?: string;
+  foiAtendido?: boolean;
+  foiAgendado?: boolean;
+}
+
+export function CardConsulta({
+  nome,
+  foto,
+  especialidade,
+  data,
+  foiAgendado,
+  foiAtendido,
+}: CardProps) {
+  return (
+    <VStack
+      w="100%"
+      bgColor={foiAtendido ? "blue.100" : "white"}
+      p={5}
+      borderRadius="lg"
+      shadow="2"
+    >
+      <VStack flexDir="row">
+        <Avatar source={{ uri: foto }} size="lg" />
+        <VStack pl={4}>
+          <Text fontSize="md" bold>
+            {nome}
+          </Text>
+          <Text>{especialidade}</Text>
+          {data && <Text>{data}</Text>}
+        </VStack>
+      </VStack>
+      <Botao
+        marginTop={4}
+        label={foiAgendado ? "Cancelar" : "Agendar Consulta"}
+      />
+    </VStack>
+  );
+}
