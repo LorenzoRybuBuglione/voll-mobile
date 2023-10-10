5944390f6b045c41850e20b5c2b6141d6f291a61
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-10-10 18:42:59 -0300

    Show User Data and LogOut

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,7 +1,33 @@
 import { VStack, Text, ScrollView, Avatar, Divider } from "native-base";
+import { useEffect, useState } from "react";
 import { Titulo } from "../componentes/Titulo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { pegarDadosPaciente } from "../servicos/PacienteServico";
+import { Paciente } from "../Interfaces/Paciente";
+import { Botao } from "../componentes/Botao";
 
-export default function Perfil() {
+export default function Perfil({ navigation }) {
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente);
+
+  useEffect(() => {
+    async function dadosPaciente() {
+      const pacienteId = await AsyncStorage.getItem("pacienteID");
+      if (!pacienteId) return null;
+      const resultado = await pegarDadosPaciente(pacienteId);
+      if (resultado) {
+        setDadosPaciente(resultado);
+        console.log(resultado);
+      }
+    }
+    dadosPaciente();
+  }, []);
+
+  function deslogar() {
+    AsyncStorage.removeItem("token");
+    AsyncStorage.removeItem("pacienteID");
+    navigation.replace("Login");
+  }
+
   return (
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
@@ -13,16 +39,18 @@ export default function Perfil() {
         />
         <Titulo color="blue.500">Informaçōes Pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>
-          Lorenzo Rybu Buglione
+          {dadosPaciente.nome}
         </Titulo>
-        <Text>6 de Junho de 2000</Text>
-        <Text>São Paulo</Text>
+        <Text>{dadosPaciente.email}</Text>
+        <Text>{dadosPaciente.endereco?.estado}</Text>
         <Divider />
         <Titulo color="blue.500" mb={1}>
-          Histórico Médico
+          Planos de Saúde
         </Titulo>
-        <Text>Sinusite</Text>
-        <Text>Bronquite</Text>
+        {dadosPaciente.planosSaude?.map((plano, index) => (
+          <Text key={index}>{plano}</Text>
+        ))}
+        <Botao label="Deslogar" onPress={deslogar} />
       </VStack>
     </ScrollView>
   );
